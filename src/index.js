1abaff0ae871a427a0fe146e1d24df9ef9f97efc
Alex Iriarte <iriarte.alex77@gmail.com>
2022-10-13 17:39:32 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Module from './Module';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.min.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Module />
     <App />
   </React.StrictMode>
 );
